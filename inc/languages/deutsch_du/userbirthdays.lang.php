b058d97764724a04f163d45744b33928b7dc74c9
Lara <75126217+little-evil-genius@users.noreply.github.com>
2025-05-23 20:37:25 +0200

    Create userbirthdays.lang.php

---
+++ inc/languages/deutsch_du/userbirthdays.lang.php
@@ -0,0 +1,20 @@
+<?php
+$l['userbirthdays_index_headline'] = "Herzlichen Glückwunsch zum Geburtstag, {1}!";
+$l['userbirthdays_banner_team_single'] = "Heute hat {1} Geburtstag!";
+$l['userbirthdays_banner_team_plural'] = ">Heute haben <b>{1}</b> User:innen Geburtstag! Und zwar: {2}";
+
+$l['userbirthdays_lists'] = "Listen";
+$l['userbirthdays_list'] = "Geburtstage der User:innen";
+
+$l['userbirthdays_jan'] = "Januar";
+$l['userbirthdays_feb'] = "Februar";
+$l['userbirthdays_mar'] = "März";
+$l['userbirthdays_apr'] = "April";
+$l['userbirthdays_mai'] = "Mai";
+$l['userbirthdays_jun'] = "Juni";
+$l['userbirthdays_jul'] = "Juli";
+$l['userbirthdays_aug'] = "August";
+$l['userbirthdays_sep'] = "September";
+$l['userbirthdays_okt'] = "Oktober";
+$l['userbirthdays_nov'] = "November";
+$l['userbirthdays_dez'] = "Dezember";
